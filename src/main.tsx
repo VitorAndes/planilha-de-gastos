2398231c8c7b0267c7b54a06e4ea31f67b4c1453
Vitor andes <vitor.andes.santos04@gmail.com>
2024-11-07 18:41:21 -0400

    :rocket: deploy: teste de analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Toaster } from 'sonner'
-import { App } from './App.tsx'
-import './index.css'
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
+import { App } from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <Toaster richColors visibleToasts={2} theme='dark' expand toastOptions={{
-      className: "p-5 text-base",
-    }}/>
-  </StrictMode>,
-)
+    <Toaster
+      richColors
+      visibleToasts={2}
+      theme="dark"
+      expand
+      toastOptions={{
+        className: "p-5 text-base",
+      }}
+    />
+    <Analytics />
+  </StrictMode>
+);
