5f7be41b319be9ffffb7fd2982b5406fe0023b9d
Vitor andes <vitor.andes.santos04@gmail.com>
2024-11-16 23:44:14 -0400

    :bug: - removing stripe for tests

--- src/components/buyMeACoffee.tsx
+++ src/components/buyMeACoffee.tsx
@@ -1,16 +1,23 @@
 import { Coffee } from "lucide-react";
+import { toast } from "sonner";
 import { Button } from "./ui/button";
 
 export function BuyMeACoffee() {
+  function commingSoon() {
+    toast.info("disponivel em breve.");
+  }
   return (
     <Button
       variant={"outline"}
+      onClick={commingSoon}
       className="md:text-lg text-color-text dark:bg-color-accent bg-color-primary md:p-6 transition-all"
     >
       <a
         className="flex items-center gap-2"
-        href="https://donate.stripe.com/6oE16l3PMf4o9cA8ww"
-        target="_blank"
+        href="#soon"
+        // href="https://donate.stripe.com/6oE16l3PMf4o9cA8ww"
+        // target="_blank"
+        aria-disabled
         rel="noreferrer"
       >
         Buy me a coffee <Coffee />
